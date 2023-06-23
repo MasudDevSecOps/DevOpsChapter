c1cf8df160b4dc883960657f5a97aa6038c5f02c
MasudDevSecOps <rahmanmdm16@gmail.com>
2023-06-22 22:12:08 -0500

    Adding the python practice session

---
+++ Python/variables.py
@@ -0,0 +1,9 @@
+# Variable assignment
+name = "John"
+age = 30
+is_student = True
+
+# Data types
+print(type(name))       # Output: <class 'str'>
+print(type(age))        # Output: <class 'int'>
+print(type(is_student)) # Output: <class 'bool'>
